e3b100ed63427fa2c8257de5f310fe581e37681f
Afif Hasan Ranasa <161363532+afif-hasan@users.noreply.github.com>
2024-05-09 08:13:22 +0600

    Update B_Rock_and_Lever.cpp

--- week 4/day 1/B_Rock_and_Lever.cpp
+++ week 4/day 1/B_Rock_and_Lever.cpp
@@ -1,33 +1,36 @@
 #include <bits/stdc++.h>
+#define endl '\n'
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long int
+#define loop for (ll i = 0; i < n; i++)
+#define tst     \
+    ll tst;     \
+    cin >> tst; \
+    while (tst--)
 using namespace std;
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int t;
-    cin >> t;
-    while (t--)
+    fast;
+    tst
     {
-        int n;
+        ll n;
         cin >> n;
-        map<int, int> cnt;
-        for (int i = 0; i < n; i++)
+        vector<ll> v(n);
+        map<ll, ll> mp;
+        for (ll i = 0; i < n; i++)
         {
-            int x;
-            cin >> x;
-            cnt[__lg(x)]++;
+            cin >> v[i];
+            mp[__lg(v[i])]++; // last bit er 2 er power koto sheta bole dey
+            // or postition of the last bit tao bole dey.O(1) complexity
         }
-
-        long long ans = 0;
-        for (auto [x, y] : cnt)
+        ll ans = 0;
+        for (auto [x, y] : mp) // evbe map er value gula access kora jay
         {
-            ans += (1LL * y * (y - 1)) / 2;
+            ans += (y * (y - 1)) / 2;// pairs can be made from n elements
         }
-
-        cout << ans << '\n';
+        cout << ans << endl;
     }
-
     return 0;
 }
