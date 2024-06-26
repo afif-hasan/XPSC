4b20d7c1ebcb93ca2ff5c35b28b8a5d7bef4f370
Afif Hasan Ranasa <161363532+afif-hasan@users.noreply.github.com>
2024-06-26 23:57:18 +0600

    Add files via upload

---
+++ week 6/day 4/F_String_Game.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long int
+#define loop for (ll i = 0; i < n; i++)
+#define tst     \
+    ll tst;     \
+    cin >> tst; \
+    while (tst--)
+using namespace std;
+int main()
+{
+    fast;
+    string t, p;
+    cin >> t;
+    cin >> p;
+    int n = t.size(), m = p.size();
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    auto ok = [&](int mid)
+    {
+        vector<bool> bad(n + 1);
+        for (int i = 0; i < mid; i++)
+        {
+            bad[a[i]] = true;
+        }
+        int j = 0;
+        bool found = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (t[i] == p[j] && !bad[i + 1])
+            {
+                j++;
+            }
+            if (j == m)
+            {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    };
+
+    int l = 0, r = n, mid, ans = 0;
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+        if (ok(mid))
+        {
+            ans = mid;
+            l = mid + 1;
+        }
+        else
+        {
+            r = mid - 1;
+        }
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
