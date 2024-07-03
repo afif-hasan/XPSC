edd283c99c563c1dde9d07213643df035b849fd2
Afif Hasan Ranasa <161363532+afif-hasan@users.noreply.github.com>
2024-07-03 10:35:51 +0600

    Update D_Children_Holiday.cpp

--- week 6/day 4/D_Children_Holiday.cpp
+++ week 6/day 4/D_Children_Holiday.cpp
@@ -10,56 +10,56 @@
     cin >> tst; \
     while (tst--)
 using namespace std;
+
+bool possible(ll mid, ll b, ll n, vector<tuple<int, int, int>> person, vector<ll> &result)
+{
+    ll total_ballons = 0;
+    ll baki = b;
+    vector<ll> can(n);
+    for (int i = 0; i < n; i++)
+    {
+        ll perballonTime = get<0>(person[i]);
+        ll restperballon = get<2>(person[i]);
+        ll ability = get<1>(person[i]);
+        ll totalTimeInOneAttempt = (perballonTime * ability) + restperballon;
+        ll fullCycle = (mid / totalTimeInOneAttempt);
+        ll timeremain = mid % totalTimeInOneAttempt;
+        ll total_ballons = fullCycle * ability + min(ability, timeremain / perballonTime);
+        total_ballons = min(baki, total_ballons);
+        can[i] = total_ballons;
+        baki -= total_ballons;
+    }
+    if (baki == 0) // balloons sesh
+    {
+        for (ll i = 0; i < n; i++)
+        {
+            result[i] = can[i];
+        }
+    }
+    if (baki == 0) // balloons sesh
+        return true;
+    else
+        return false;
+}
 int main()
 {
     fast;
-    int m, n;
-    cin >> m >> n;
-    vector<tuple<int, int, int>> a(n);
-    for (int i = 0; i < n; i++)
+    ll no_of_ballons, no_of_assistnt;
+
+    cin >> no_of_ballons >> no_of_assistnt;
+    vector<tuple<int, int, int>> person(no_of_assistnt);
+    for (ll i = 0; i < no_of_assistnt; i++)
     {
-        int t, z, y;
-        cin >> t >> z >> y;
-        a[i] = make_tuple(t, z, y);
+        ll ti, ability, rest;
+        cin >> ti >> ability >> rest;
+        person[i] = make_tuple(ti, ability, rest);
     }
-
-    vector<int> can(n);
-    auto ok = [&](int mid)
-    {
-        vector<int> may(n);
-        int has = m;
-        for (int i = 0; i < n; i++)
-        {
-            tuple<int, int, int> T = a[i];
-            int t, z, y, total, can, remaining, curr;
-            t = get<0>(T);
-            z = get<1>(T);
-            y = get<2>(T);
-            total = ((t * z) + y);
-            can = (mid / total);
-            remaining = mid % total;
-            curr = ((can * z) + min(z, (remaining / t)));
-            curr = min(has, curr);
-            has -= curr;
-            may[i] = curr;
-        }
-
-        if (!has)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                can[i] = may[i];
-            }
-        }
-
-        return (!has);
-    };
-
-    int l = 0, r = 1e8, mid, ans;
+    ll l = 0, r = 1e8, ans = -1;
+    vector<ll> result(no_of_assistnt);
     while (l <= r)
     {
-        mid = l + (r - l) / 2;
-        if (ok(mid))
+        ll mid = (l + r) / 2;
+        if (possible(mid, no_of_ballons, no_of_assistnt, person, result))
         {
             ans = mid;
             r = mid - 1;
@@ -70,11 +70,10 @@ int main()
         }
     }
 
-    cout << ans << '\n';
-    for (auto val : can)
+    cout << ans << endl;
+    for (ll val : result)
     {
         cout << val << " ";
     }
-    cout << '\n';
     return 0;
 }
