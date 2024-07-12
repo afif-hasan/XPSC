ea292cf97ef9cdc3632d1120dc862833b8b67af5
Afif Hasan Ranasa <161363532+afif-hasan@users.noreply.github.com>
2024-07-12 23:50:01 +0600

    Update B_Array_Decrements.cpp

--- week 7/day 2/B_Array_Decrements.cpp
+++ week 7/day 2/B_Array_Decrements.cpp
@@ -1,48 +1,51 @@
 #include <bits/stdc++.h>
-
-#define forn(i, n) for (int i = 0; i < int(n); i++)
-
+#define endl '\n'
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long int
+#define loop for (ll i = 0; i < n; i++)
+#define tst     \
+    ll tst;     \
+    cin >> tst; \
+    while (tst--)
 using namespace std;
-const int inf = 1e9 + 7;
-
-bool equals(vector<int> &a, vector<int> &b, int n)
-{
-    int dif = inf;
-    forn(i, n)
-    {
-        if (b[i] != 0)
-            dif = min(dif, a[i] - b[i]);
-    }
-    if (dif < 0)
-        return false;
-    if (dif == inf)
-        return true;
-    forn(i, n)
-    {
-        if (a[i] - b[i] > dif)
-            return false;
-        if (b[i] != 0 && a[i] - b[i] < dif)
-            return false;
-    }
-    return true;
-}
-
-void solve()
-{
-    int n;
-    cin >> n;
-    vector<int> a(n), b(n);
-    forn(i, n) cin >> a[i];
-    forn(i, n) cin >> b[i];
-    cout << (equals(a, b, n) ? "YES\n" : "NO\n");
-}
-
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    fast;
+    tst
     {
-        solve();
+        ll n;
+        cin >> n;
+        vector<ll> a(n), b(n);
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+        ll mindif = LLONG_MAX;
+        for (ll i = 0; i < n; i++)
+        {
+            if (b[i] != 0)
+                mindif = min(mindif, a[i] - b[i]);
+        }
+        if (mindif < 0)
+            cout << "NO" << endl;
+        else
+        {
+            vector<ll> res;
+            for (ll i = 0; i < n; i++)
+            {
+                res.push_back(max((a[i] - mindif), 0LL));
+            }
+            if (res == b)
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
+        }
     }
+    return 0;
 }
