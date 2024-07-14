036a030b05a1de66e93842f6316278ab3af71ce1
Afif Hasan Ranasa <161363532+afif-hasan@users.noreply.github.com>
2024-07-14 13:22:24 +0600

    Update C_Grandma_Capa_Knits_a_Scarf.cpp

--- week 7/day 2/C_Grandma_Capa_Knits_a_Scarf.cpp
+++ week 7/day 2/C_Grandma_Capa_Knits_a_Scarf.cpp
@@ -1,47 +1,110 @@
 #include <bits/stdc++.h>
-
+#define endl '\n'
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long int
+#define loop for (ll i = 0; i < n; i++)
+#define tst     \
+    ll tst;     \
+    cin >> tst; \
+    while (tst--)
 using namespace std;
-
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int t, n;
-    cin >> t;
-    while (t--)
+    fast;
+    tst
     {
+        ll n;
+        cin >> n;
         string s;
-        cin >> n >> s;
-        int ans = n + 1;
-        for (int c = 0; c < 26; ++c)
+        cin >> s;
+        ll i = 0, j = n - 1;
+        ll cnt = 0;
+        bool flag = true;
+        while (i <= j)
         {
-            int l = 0, r = n - 1, cnt = 0;
-            while (l <= r)
+            char omil;
+            if (s[i] != s[j])
             {
-                if (s[l] == s[r])
+                if (cnt == 0)
                 {
-                    l++, r--;
-                }
-                else if (s[l] == char('a' + c))
-                {
-                    cnt++, l++;
-                }
-                else if (s[r] == char('a' + c))
-                {
-                    cnt++, r--;
+                    omil = s[i];
+                    cnt++;
+                    i++;
                 }
                 else
                 {
-                    cnt = n + 1;
-                    break;
+                    if (s[i] == omil)
+                    {
+                        cnt++;
+                        i++;
+                    }
+                    else if (s[j] == omil)
+                    {
+                        cnt++;
+                        j--;
+                    }
+                    else
+                    {
+                        flag = false;
+                        break;
+                    }
                 }
             }
-            ans = min(ans, cnt);
+            else
+            {
+                i++;
+                j--;
+            }
         }
-        if (ans == n + 1)
-            ans = -1;
-        cout << ans << '\n';
+        i = 0, j = n - 1;
+        ll cnt2 = 0;
+        bool flag2 = true;
+        while (i <= j)
+        {
+            char omil2;
+            if (s[i] != s[j])
+            {
+                if (cnt2 == 0)
+                {
+                    omil2 = s[j]; // change
+                    cnt2++;
+                    j--;
+                }
+                else
+                {
+                    if (s[i] == omil2)
+                    {
+                        cnt2++;
+                        i++;
+                    }
+                    else if (s[j] == omil2)
+                    {
+                        cnt2++;
+                        j--;
+                    }
+                    else
+                    {
+                        flag2 = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                i++;
+                j--;
+            }
+        }
+        if (flag && flag2)
+            cout << min(cnt, cnt2) << endl;
+        else if (flag)
+            cout << cnt << endl;
+        else if (flag2)
+            cout << cnt2 << endl;
+        else
+            cout << -1 << endl;
     }
     return 0;
 }
