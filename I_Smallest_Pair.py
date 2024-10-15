03a55e28635cbf22606d0976d4498c9c62806b40
Afif Hasan Ranasa <161363532+afif-hasan@users.noreply.github.com>
2024-10-15 08:49:32 +0600

    Delete I_Smallest_Pair.py

--- I_Smallest_Pair.py
+++
@@ -1,9 +0,0 @@
-tst=int(input())
-for t in range(tst):
-    n=int(input())
-    numbers=list(map(int,input().split()))
-    mn= 1e18+2
-    for i in range(0,n-1):
-            for j in range(i+1,n):
-                mn=min(mn,numbers[i]+numbers[j]+j-i)
-    print(mn)
